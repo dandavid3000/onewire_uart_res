d47038557bcdd4887a2ff6e22628a4f80a392cdc
tilz0R <tilen.majerle@gmail.com>
2018-08-30 23:20:11 +0200

    Correct example code for new ROM variable

--- docs/examples/ow_search.c
+++ docs/examples/ow_search.c
@@ -1,17 +1,17 @@
 ow_t ow;
 owr_t res;
-uint8_t rom_id[8];
+ow_rom_t rom_id;
 
 ow_init(&ow, NULL);
 
 ow_protect(&ow, 1);
-res = ow_search_reset(&ow, 0);
+res = ow_search_reset_raw(&ow, 0);
 /* Search until all devices scanned */
-while (res == owOK && (res = ow_search(&ow, rom_id, 0)) == owOK) {
+while (res == owOK && (res = ow_search_raw(&ow, &rom_id, 0)) == owOK) {
 	printf("New device found!\r\n");
     printf("ID: %02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\r\n",
-        rom_id[0], rom_id[1], rom_id[2], rom_id[3],
-        rom_id[4], rom_id[5], rom_id[6], rom_id[7],
+        (unsigned)rom_id.rom[0], (unsigned)rom_id.rom[1], (unsigned)rom_id.rom[2], (unsigned)rom_id.rom[3],
+        (unsigned)rom_id.rom[4], (unsigned)rom_id.rom[5], (unsigned)rom_id.rom[6], (unsigned)rom_id.rom[7]
     );
 }
 ow_unprotect(&ow, 1);
