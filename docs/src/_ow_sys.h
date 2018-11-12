f2e4e579f57ed2060dccbae9efb641f7cd1f1084
tilz0R <tilen.majerle@gmail.com>
2018-11-12 21:55:09 +0100

    Added CMSIS os docs example

---
+++ docs/src/_ow_sys.h
@@ -0,0 +1,13 @@
+/**
+ * \addtogroup      OW_SYS
+ * \{
+ *
+ * System functions, required when operating system mode is enabled.
+ *
+ * \par             Example for CMSIS OS
+ *
+ * \example         ow_sys.c
+ *
+ * \}
+ *
+ */
